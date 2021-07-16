1ad3858f96abc436c92373c1c71a3defb02bfb65
praveenkumar021 <pravee.nku04@gmail.com>
2021-07-16 23:43:05 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kumar
 # kumar
+# kumar
---
+++ src/org/Test/Sample.java
@@ -0,0 +1,12 @@
+package org.Test;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		System.out.println("praveen");
+		System.out.println("abu");
+		System.out.println("ravi");
+		System.out.println("pk");
+	}
+
+}
